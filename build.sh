77b20cbcb8635439986743e025be68665a662406
hiromi-mi <hiromi-mi@cat.zaq.jp>
2021-06-07 00:07:54 +0900

    add lzma_sdk patch

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-wget https://chromium.googlesource.com/chromium/src/+archive/refs/heads/main/third_party/lzma_sdk.tar.gz && tar xf -C third_party/ lzma_sdk.tar.gz
+mkdir -p third_party/lzma_sdk
+wget https://chromium.googlesource.com/chromium/src/+archive/refs/heads/main/third_party/lzma_sdk.tar.gz && tar xf -C third_party/lzma_sdk/ lzma_sdk.tar.gz
+cd third_party/lzma_sdk/ && patch -p0 < ../../lzma_sdk.patch && cd ../..
 python gn/build/gen.py
 ninja -C ./gn/out
 ./gn/out/gn gen -C out/Default
